8bd7826546a2e3c1aef72b0c15368e8b49bd7144
Boud <maxime.boudreault.0@gmail.com>
2017-12-04 20:03:30 -0500

    Add moving spaceship

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Spaceship))]
+public class PlayerController : MonoBehaviour
+{
+    const string HORIZONTAL = "{0}Horizontal";
+    const string VERTICAL = "{0}Vertical";
+
+    [SerializeField] string controllerId = "P0";
+
+    Spaceship spaceship;
+
+    void Start()
+    {
+        spaceship = GetComponent<Spaceship>();
+    }
+
+    void FixedUpdate()
+    {
+        spaceship.MoveTowardRelative(
+            GetAxis(HORIZONTAL),
+            GetAxis(VERTICAL)
+        );
+    }
+
+    float GetAxis(string format)
+    {
+        return Input.GetAxis(string.Format(format, controllerId));
+    }
+}
---
+++ Assets/Spaceships/Spaceship.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Spaceship : MonoBehaviour
+{
+    [SerializeField] float maximumSpeed;
+    [SerializeField] float thrusterStrength;
+
+    Rigidbody body;
+    float sqrMaximumSpeed;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+        sqrMaximumSpeed = maximumSpeed * maximumSpeed;
+    }
+
+    public void MoveToward(float x, float y)
+    {
+        Vector3 direction = (new Vector3(x, y, body.position.z) - body.position).normalized;
+        body.AddForce(direction * thrusterStrength);
+    }
+
+    public void MoveTowardRelative(float x, float y)
+    {
+        Vector3 direction = (new Vector3(x, y)).normalized;
+        body.AddForce(direction * thrusterStrength);
+    }
+
+    public void FixedUpdate()
+    {
+        if (body.velocity.sqrMagnitude > sqrMaximumSpeed)
+            body.velocity = body.velocity.normalized * maximumSpeed;
+    }
+}
