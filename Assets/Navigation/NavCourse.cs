7620b644b9801b1b9e7ab8f7e2d8ea596bb5bd6d
Boud <maxime.boudreault.0@gmail.com>
2017-12-05 19:45:26 -0500

    Add NavCourse for level editing

---
+++ Assets/Navigation/NavCourse.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class NavCourse : MonoBehaviour
+{
+    [SerializeField] NavPoint[] steps;
+
+    void Awake()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(transform.position, 2f);
+
+        Gizmos.DrawLine(transform.position, steps[0].Position);
+        for (int i = 1; i < steps.Length; i++)
+            Gizmos.DrawLine(steps[i - 1].Position, steps[i].Position);
+    }
+
+    public bool TryGetStep(int index, out Vector3 step)
+    {
+        if (index > steps.Length || index < 0)
+        {
+            step = steps[steps.Length - 1].Position;
+            return false;
+        }
+
+        if (index == 0)
+            step = transform.position;
+        else
+            step = steps[index - 1].Position;
+        return true;
+    }
+}
---
+++ Assets/Navigation/NavPoint.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class NavPoint : MonoBehaviour
+{
+    public Vector3 Position { get { return transform.position; } }
+
+    void Awake()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(transform.position, 1.5f);
+    }
+}
