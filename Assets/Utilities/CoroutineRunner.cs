8eafcce5790da7647a297b6c5a3670b8e15c32b7
Boud <maxime.boudreault.0@gmail.com>
2017-12-08 12:36:40 -0500

    Add promise like utility

---
+++ Assets/Utilities/CoroutineRunner.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CoroutineRunner : MonoBehaviour
+{
+    public Coroutine Run(IEnumerator coroutine)
+    {
+        return StartCoroutine(coroutine);
+    }
+
+    public void Stop(Coroutine coroutine)
+    {
+        StopCoroutine(coroutine);
+    }
+}
