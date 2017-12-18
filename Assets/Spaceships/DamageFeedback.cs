539121c6dbd61128560ba6f769bae08415097bd0
Boud <maxime.boudreault.0@gmail.com>
2017-12-18 13:01:46 -0500

    Add damage feedbacl

---
+++ Assets/Spaceships/DamageFeedback.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Life))]
+[RequireComponent(typeof(MeshRenderer))]
+public class DamageFeedback : MonoBehaviour
+{
+    new MeshRenderer renderer;
+    Color baseColor;
+    float intensity;
+
+    void Start()
+    {
+        var life = GetComponent<Life>();
+        life.OnChange += CheckForFeedback;
+
+        renderer = GetComponent<MeshRenderer>();
+        baseColor = renderer.materials[1].color;
+    }
+
+    void CheckForFeedback(float oldLife, float newLife)
+    {
+        if (newLife < oldLife)
+            intensity = 1;
+    }
+
+    void Update()
+    {
+        baseColor.a = intensity;
+        renderer.materials[1].color = baseColor;
+        intensity -= Time.deltaTime;
+    }
+}
